d0fa4d10b8ced2bf4c7bf0c8ac98e9fa40aaa0ca
Slavek Jurkowski <slavek.jurkowski@voyagersopris.com>
2016-03-11 16:11:05 -0600

    add

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "sjurkowski"
+client_key               "#{current_dir}/sjurkowski.pem"
+validation_client_name   "voysop_td-validator"
+validation_key           "#{current_dir}/voysop_td-validator.pem"
+chef_server_url          "https://dalvslbuild.clg.local:446/organizations/voysop_td"
+cookbook_path            ["#{current_dir}/../cookbooks"]
